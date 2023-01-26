d01e2bc1cec4e97267de0aa0a69f8f63ec9975c8
Mason Rhodes <mason.rhodes@outlook.com>
2023-01-26 13:09:07 -0800

    fix: add cors plugin

--- server/src/services/http/index.ts
+++ server/src/services/http/index.ts
@@ -1,9 +1,15 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors'
 
-export const fastify = Fastify();
+export const fastify = Fastify({logger: true});
 
 export const startFastify = async () => {
-    await fastify.listen({ port: 3000 });
+    await fastify.register(cors, { 
+        // put your options here
+      })
+    await fastify.listen({ 
+        port: 3000,
+    });
     const address = fastify.server.address();
     const port = typeof address === "string" ? address : address?.port;
     console.log(`Server listening on http://localhost:${port}`);
