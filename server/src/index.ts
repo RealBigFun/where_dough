20f52024451012b1715250644bbf0b9c539234e9
Mason Rhodes <mason@lifemd.com>
2023-01-28 11:28:23 -0800

    feat: promote web to root

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { startApolloServer } from './services/apollo'
-import { startFastify } from './services/http';
-
-async function main() {
-    await startApolloServer();
-    await startFastify();
-}
-
-main();
