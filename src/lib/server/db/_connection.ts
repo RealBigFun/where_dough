6a695b9e8316f3fadcee199fb2c28bdc842eeecd
Mason Rhodes <mason.rhodes@outlook.com>
2023-02-01 10:39:10 -0800

    Update _connection.ts

--- src/lib/server/db/_connection.ts
+++ src/lib/server/db/_connection.ts
@@ -8,9 +8,6 @@ let session: Session | null = null
 export function getDriver() {
     if (!driver) {
         const { NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD } = getConfig()
-        console.log('Connecting to Neo4j at', NEO4J_URI)
-        console.log('Using Neo4j user', NEO4J_USER)
-        console.log('Using Neo4j password', NEO4J_PASSWORD)
         driver = neo4j.driver(NEO4J_URI, neo4j.auth.basic(NEO4J_USER, NEO4J_PASSWORD))
     }
     return driver
